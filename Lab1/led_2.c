c074a8462ce0741cca574e07615797f8c48ac321
RavinduSuraweeraarachchi <e19393@eng.pdn.ac.lk>
2025-05-08 18:57:17 +0530

    Added Comments

--- Lab1/led_2.c
+++ Lab1/led_2.c
@@ -7,18 +7,30 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+// Define the delay for the blinking
 #define BLINK_DELAY 1000
 
 int main (void){
-    // DDRB = DDRB|(0b1111<<2);
-    DDRB = (0b1111<<2);
+    // Data Direction Register B (DDRB) is used to set the direction of the pins
+    // Set the pins for the LEDs as output
+    // Here, we try to use PB2, PB3, PB4, and PB5
+    // Corresponding Pins in Arduino: 10, 11, 12, and 13 (Digital Pins)
+    DDRB = DDRB | (0b1111<<2);
+
+    // The forever loop to blink the LEDs
     while(1){
-        //PORTB = PORTB|(0b1111<<2);
-        PORTB = (0b1111<<2);
+        // Now that the pins are set as output, we can turn on the LEDs
+        // The PB2, PB3, PB4, and PB5 pins are set to HIGH, and wait for BLINK_DELAY
+        PORTB = PORTB | (0b1111<<2);
         _delay_ms(BLINK_DELAY);
 
-        //PORTB = PORTB &~(0b1111<<2);
-        PORTB = (0b0000<<2);
+        // Now turn off the said LEDs
+        // The PB2, PB3, PB4, and PB5 pins are set to LOW, and wait for BLINK_DELAY
+        PORTB = PORTB & ~(0b1111<<2);
         _delay_ms(BLINK_DELAY);
+
     }
+
+    // Return 0 to indicate successful completion
+    return 0;
 }
