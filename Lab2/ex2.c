4ce538fc9c1c90b5fc4c9339bd6ab8493015e089
RavinduSuraweeraarachchi <e19393@eng.pdn.ac.lk>
2025-05-21 14:52:25 +0530

    Final Changes

--- Lab2/ex2.c
+++ Lab2/ex2.c
@@ -14,16 +14,27 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-// We can write the main function to implement the toggle logic
+// Here we define a volatile variable to indicate button release
+// This is required to indicate whether the button has been released
+volatile uint8_t button_released = 0;
+
+// Let us define the main function to implement the toggle logic
 int main()
 {
     // Let us use PD2 as the input pin for the button
     // We need to set the respective pin as input
     DDRD &= ~(1 << PD2);
 
+    // We can enable the pull-up resistor on PD2
+    PORTD |= (1 << PD2);
+
     // Now we can set the output pin
     // Let us use PB0 as the output pin
-    DDRB |= (1 << PB0); // Set PB0 as output
+    DDRB |= (1 << PB0);
+
+    // Initialize the PB0 to 0
+    // This will turn off the LED initially
+    PORTB &= ~(1 << PB0);
 
     // Since the logic should work whenever the button is released,
     // We need to set the INT0 to trigger on the falling edge
@@ -33,34 +44,45 @@ int main()
     EICRA |= (1 << ISC01); 
     EICRA &= ~(1 << ISC00);
 
-    // Now we can enable global interrupts to allow the interrupt to be triggered
-    // For this we can use the sei() macro which sets the global interrupt enable bit
-    // Global interrupt enable bit is in the SREG register (bit 7)
-    sei();
-
     // Now we can enable the INT0 external interrupt
     // For this we need to set the EIMSK (External Interrupt Mask Register)
-    // We need to set the INT0 bit to 1, which the pin PD2 is connected to
     EIMSK |= (1 << INT0);
+    
+    // Enable global interrupts
+    sei();
 
     // Now we can enter an infinite loop
     while (1)
-    {
-        // Do nothing, just wait for the interrupt to be triggered
+    {   
+        // Continuously check if the button has been released or not
+        if (button_released) {
+            // If the button is released, toggle the LED
+            PORTB ^= (1 << PB0);
+            
+            // After toggling, reset the button_released flag
+            button_released = 0;
+            
+            // Wait for button to be fully released
+            while (!(PIND & (1 << PD2))) {
+                _delay_ms(1);
+            }
+            
+            // Small debounce delay after release
+            _delay_ms(20);
+        }
     }
     return 0;
 }
 
 // Now we can define the ISR (Interrupt Service Routine) for INT0
 // This is the function that will be called when the interrupt is triggered
-
-ISR (INT0_vect)
+ISR(INT0_vect)
 {
-    // Toggle the LED connected to PB0
-    // To toggle the LED, we can use the XOR operator
-    // What XOR does is it flips the bit
-    PORTB ^= (1 << PB0);
-
-    // Add a small delay to debounce the button
-    _delay_ms(100); 
+    // Simple debounce delay
+    _delay_ms(20);
+    
+    // Verify button is still pressed (active low)
+    if (!(PIND & (1 << PD2))) {
+        button_released = 1;
+    }
 }
--- Lab2/ex3_2.c
+++ Lab2/ex3_2.c
@@ -1,38 +1,73 @@
+// CO321 Embedded Systems
+// Lab 02: AVR Interrupt Programming in C
+// Exercise 03: Binary Counter with Interrupt
+// Group 09
+
+// Question:
+// PD7 pin is connected to a push button. 
+// Write a program that uses 6 LEDs connected to PORTB (6 LSBs) 
+// to display the number of times the push button is pressed, 
+// as a binary number. 
+// Extend the program so that the number of times the push button was released
+// is displayed as a binary number on a set of LEDs connected to PORTB
+
+// NOTE: The code is configured for an Active LOW button configuration.
+
+// First, we need to include the necessary header files
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-volatile uint8_t buttonPressCount = 0;  // Counter variable
+// A volatile variable to store the button press count
+// This is required to count the number of times the button is pressed
+// and is declared as volatile because it is modified in the ISR
+// and accessed in the main loop
+volatile uint8_t buttonPressCount = 0;  
 
+// Let us define the main function to implement the counter logic
 int main()
 {
-    // Set PD2 as input (button) with pull-up
+    // Since we intend to use PD2 as input, we need to set it as input
     DDRD &= ~(1 << PD2);
+
+    // Also, we can enable the pull-up resistor on PD2
     PORTD |= (1 << PD2);
     
-    // Set PORTB as output (6 LSBs for LEDs)
+    // Since we need to use PORTB for the LEDs, we need to set it as output
     DDRB |= 0b00111111;
+
+    // For the counting, we can initialize PORTB to 0
     PORTB = 0;
 
-    // Configure INT0 to trigger on falling edge (button press)
+    // Again, we count the # of times the button is released
+    // Hence, we need to set INT0 to trigger on the falling edge
+    // For this we need to look at EICRA (External Interrupt Control Register A)
+    // According to the datasheet, we need to set the ISC01 bit to 1
+    // and ISC00 bit to 0
     EICRA |= (1 << ISC01);
     EICRA &= ~(1 << ISC00);
     
-    // Enable INT0 interrupt
+    // Now we can enable the INT0 external interrupt
+    // For this we need to set the EIMSK (External Interrupt Mask Register)
+    // We need to set the INT0 bit to 1, which the pin PD2 is connected to
     EIMSK |= (1 << INT0);
     
-    // Enable global interrupts
+    // Now we can enable global interrupts to allow the interrupt to be triggered
     sei();
 
+    // In this infinite loop, 
     while (1) 
     {
-        // Continuously update the display
+        // Here, we continuously display the current binary value
+        // The binary value will be updated in the ISR when a button is pressed
         PORTB = buttonPressCount & 0b00111111;
     }
 
     return 0;
 }
 
+// Now we can define the ISR (Interrupt Service Routine) for INT0
+// This is the function that will be called when the interrupt is triggered
 ISR(INT0_vect)
 {
     // Immediately disable interrupt to prevent re-triggering
@@ -44,12 +79,6 @@ ISR(INT0_vect)
     // Verify button is still pressed (active low)
     if (!(PIND & (1 << PD2))) {
         buttonPressCount++;
-        
-        // Wait for button release before re-enabling interrupt
-        // This prevents counting multiple times during long press
-        // while (!(PIND & (1 << PD2))) {
-        //     _delay_ms(10);
-        // }
     }
     
     _delay_ms(20); // Additional debounce delay
